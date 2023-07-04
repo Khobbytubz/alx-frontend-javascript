535266b4b16cb2ac5e8a1d705368486b8fe392b3
khobbytubz <khobbytubz@gmail.com>
2023-07-04 18:23:17 +0000

    get students

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
-// return an array of objects who are locaated in a specific city/location
-
-export default function getStudentsByLocation(arr, city) {
-  return arr.filter((obj) => city === obj.location);
+export default function getStudentsByLocation(students, city) {
+  return students.filter((obj) => obj.location === city);
 }
